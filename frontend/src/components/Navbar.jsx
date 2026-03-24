a82ada1f8b0da2c0477488ed7a79b1b91a6815c9
Bharath <bharathkondasani2005@gmail.com>
2026-03-25 02:49:26 +0530

    frontend update - chakra UI + create product page

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,44 @@
+﻿import { Button, Container, Flex, HStack, Text, useColorMode} from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import { CiSquarePlus } from "react-icons/ci";
+
+const Navbar = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  return (
+    <Container maxW={"1140px"} px={4}>
+      <Flex
+        h={16}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+        flexDir={{
+          base: "column",
+          sm: "row",
+        }}
+      >
+        <Text
+          fontSize={{ base: "22px", sm: "28px" }}
+          fontWeight={"bold"}
+          textTransform={"uppercase"}
+          textAlign={"center"}
+          bgGradient={"linear(to-r, orange.400, pink.400)"}
+          bgClip={"text"}
+        >
+          <Link to="/">Product Store 🛒</Link>
+        </Text>
+
+        <HStack spacing={2} alignItems={"center"}>
+          <Link to="/create">
+            <Button>
+              <CiSquarePlus fontSize={18} />
+            </Button>
+          </Link>
+          <Button onClick={toggleColorMode}>
+            {colorMode === "light" ? "🌙" :"🔆"}
+          </Button>
+        </HStack>
+      </Flex>
+    </Container>
+  );
+};
+
+export default Navbar;
---
+++ frontend/src/pages/CreatePage.jsx
@@ -0,0 +1,63 @@
+﻿import { use, useState } from 'react'
+import { Box, Container, Heading, VStack, useColorModeValue,Input, Button } from "@chakra-ui/react";
+import { useProductStore } from '../store/product.js';
+const CreatePage = () => {
+  const [newProduct, setNewProduct] = useState({
+    name: "",
+    price: "",
+    image: "",
+  });
+  const {createProduct}=useProductStore();
+  const handleAddProduct = async() => {
+  const {success, message} = await createProduct(newProduct);
+  console.log("Success:", success, "Message:", message);
+  };
+    return <Container maxW={"container.sm"}>
+      <VStack
+        spacing={9}
+      >
+        <Heading as="h1" size={"2xl"} textAlign={"center"} mb={7}>
+          Create Product Here
+        </Heading>
+
+        <Box
+          w={"full"} bg={useColorModeValue("white", "gray.700")}
+          p={6} rounded={"lg"} shadow={"md"}
+        >
+
+          <VStack spacing={4}>
+            <Input
+              placeholder='Product Name'
+              name='name'
+              value={newProduct.name}
+              onChange={(e) => setNewProduct({ ...newProduct, name: e.target.value })}
+            />
+
+            <Input
+              placeholder='Price'
+              name='price'
+              type='number'
+              value={newProduct.price}
+              onChange={(e) => setNewProduct({ ...newProduct, price: e.target.value })}
+            />
+
+            <Input
+              placeholder='Image URL'
+              name='image'
+              value={newProduct.image}
+              onChange={(e) => setNewProduct({ ...newProduct, image: e.target.value })}
+            />
+
+            <Button colorScheme='teal' onClick={handleAddProduct} w='full'>
+              Add Product
+            </Button>
+          </VStack>
+        </Box>
+      </VStack>
+
+
+
+    </Container>
+
+  };
+  export default CreatePage;
